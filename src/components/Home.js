3a3b5743665f785aad69cf8dc0e6de2ded4be535
Lubos Glomba <xglomba@stuba.sk>
2022-12-28 12:23:31 +0100

    styling of landing screen, idem jest

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,10 @@ const Home = () => (
         <Helmet><title>QiziQ - Domovska stranka</title></Helmet>
         <div id="home">
             <section>
+                <svg width="100" height="100">
+                    <circle cx="50" cy="50" r="40" stroke="#0066cc" stroke-width="4" fill="#0066cc" />
+                    <text x="50%" y="58%" text-anchor="middle" font-size="30" font-family="sans-serif" fill="white">?</text>
+                </svg>
                 <div>
                     <h2>QiziQ</h2>
                     <div className="play-button-container">
