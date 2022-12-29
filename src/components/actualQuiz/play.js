c56e2c9b7abcfa8b60025132e45561f5577eb599
Lubos Glomba <xglomba@stuba.sk>
2022-12-29 23:02:49 +0100

    static question 1

--- src/components/actualQuiz/play.js
+++ src/components/actualQuiz/play.js
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
+import { Helmet } from "react-helmet";
 
-class Play extends React.Component {
+
+class Play extends Component {
 
     constructor (props) {
-        super(props)
-        this.state = {
-            counter: 0
-        };
+        super(props);
     }
 
     incrementCounter = () => {
@@ -16,11 +15,43 @@ class Play extends React.Component {
     };
     render() {
         return (
-            <div>
-                <p>Hraci komponent</p>
-                <p>Pocidlo: { this.state.counter }</p>
-                <button onClick={ this.incrementCounter }>Klik</button>
-            </div>
+            <Fragment>
+                <Helmet>
+                    <title>QiziQ</title>
+                </Helmet>
+                <div className="question">
+                    <div className="lifeline-container">
+                        <p>
+                            <span className="mdi mdi-set-center mdi-24px mdi-lightbulb-fluorescent-tube-outline"></span>2
+                        </p>
+                        <p>
+                            <span className="mdi mdi-lightbulb-on-outline mdi-24px mdi-lightbulb-fluorescent-tube-outline"></span>5
+                        </p>
+                    </div>
+                    <div>
+                        <p>
+                            <span>1/15</span>
+                            21:14<span className="mdi mdi-clock-outline mdi-24px">1/5</span>
+                        </p>
+                    </div>
+                    <h5>Kto je najkrajsi na svete?</h5>
+                    <div className="options-container">
+                        <p className="option">ja</p>
+                        <p className="option">ty</p>
+                    </div>
+                    <div className="options-container">
+                        <p className="option">on</p>
+                        <p className="option">ona</p>
+                    </div>
+                    <div className="button-container">
+                        <button>Predchadzajuca</button>
+                        <button>Nasledujuca</button>
+                        <button>Vzdavam sa!</button>
+                    </div>
+
+                </div>
+
+            </Fragment>
         );
     }
 }
